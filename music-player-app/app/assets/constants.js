3ef11596c6321506f5399bc46fd1bd3277178610
dmitriymykhalchenko <dmitriymykhalchenko@gmail.com>
2020-02-04 10:24:57 +0200

    test

---
+++ music-player-app/app/assets/constants.js
@@ -0,0 +1,19 @@
+
+export const DATA = [
+  {
+    id: 'Treck2',
+    title: 'Comfort Fit - “Sorry"',
+    mp3: 'https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Comfort_Fit_-_03_-_Sorry.mp3',
+  },
+  {
+    id: 'Treck3',
+    title: 'Mildred Bailey – “All Of Me"',
+    mp3:'https://ia800304.us.archive.org/34/items/PaulWhitemanwithMildredBailey/PaulWhitemanwithMildredBailey-AllofMe.mp3'
+  },
+  {
+    id: 'Treck4',
+    title: 'Podington Bear - “Rubber Robot"',
+    mp3:'https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Podington_Bear_-_Rubber_Robot.mp3',
+
+  },
+];
